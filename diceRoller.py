f2bda5b40ecd1e5b7ab4dc8a638febceb233aa88
Patrick Conboy <rispir45@gmail.com>
2018-09-07 17:23:22 -0400

    Made diceRoller.py file that, when run, simulates one dice roll
    It simply prints a random int between 1 and 6, for now.

---
+++ diceRoller.py
@@ -0,0 +1,8 @@
+# File: diceRoller.py
+# Written by: Patrick Conboy
+# Date: 9/7/2018
+# Dice rolling program that simulates one dice roll. Just made this to practice some python fundamentals.
+
+import random
+
+print(random.randint(1,6))
