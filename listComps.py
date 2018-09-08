7036f8b3279823698a5da4d80bf11c53d513811a
Patrick Conboy <rispir45@gmail.com>
2018-09-08 19:58:30 -0400

    Did practice problem 1

--- listComps.py
+++ listComps.py
@@ -2,3 +2,8 @@
 # Written by: Patrick Conboy
 # Date: 9/8/2018
 # Practicing with list comprehensions.
+
+# Practice Problem 1
+numberList = [n for n in range(1,100) if n % 13 == 3]
+print(numberList)
+
