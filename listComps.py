494f7ae6bfa8bd507b22265ff2a68426e8422280
Patrick Conboy <rispir45@gmail.com>
2018-09-08 20:40:46 -0400

    Finished practice problem 2

--- listComps.py
+++ listComps.py
@@ -4,6 +4,13 @@
 # Practicing with list comprehensions.
 
 # Practice Problem 1
+# Write a list comprehension that returns all numbers from 1 to 100 that produce a remainder of 3 when divided by 13.
 numberList = [n for n in range(1,100) if n % 13 == 3]
 print(numberList)
-
+
+# Practice Problem 2
+# Write a list comprehension that is given a list of words and appends at the end of each word a space followed by the length. So the word "art" will become "art 3".
+words = ["hello", "goodbye", "slaton", "sigma", "chi"]
+newWords = [word + " " + str(len(word)) for word in words]
+print(newWords)
+
